3a38ae25182f3cb9640284c0d8bca7c9651e0e5d
Eric Gorelik <egorelik93@gmail.com>
2012-02-10 22:29:27 -0500

    Removed paste from requirements and paste debugging

--- wsgi.py
+++ wsgi.py
@@ -4,8 +4,6 @@ import sys
 import os
 
 import cherrypy
-from paste import evalexception
-from paste.httpexceptions import *
 
 
 import app
@@ -15,10 +13,7 @@ from app import application
 cherrypy.config.update({
         'log.error_file': 'site.log',
         'tools.staticdir.root': os.path.abspath('.')})
-
-     
-#Add development-exclusive app configuration
-application.wsgiapp.pipeline.append(('paste_exc', evalexception.middleware.EvalException))                  
+      
                  
 # Add config
 application.merge('app_dev.ini')
