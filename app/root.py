2b9fe04f6cfdeabc56665d1f10f744057787eb4e
Eric Gorelik <egorelik93@gmail.com>
2012-02-11 10:12:59 -0500

    Began Facebook Connect API functions

--- app/root.py
+++ app/root.py
@@ -1,5 +1,10 @@
+import json
+
+from urllib2 import urlopen
+
 import cherrypy
 from cherrypy import expose
+
 
 from genshi.template import TemplateLoader
 loader = TemplateLoader('app/view/templates', auto_reload=True)
@@ -11,7 +16,42 @@ class Root(object):
     tmpl = loader.load('index.html')
     page = tmpl.generate(title='Inspektor')
     return page.render('html', doctype='html')
-    
+
+
+  '''
+  @expose
+  def login():
+    raise cherrypy.HTTPRedirect("https://www.facebook.com/dialog/oauth?
+     client_id=YOUR_APP_ID&redirect_uri=YOUR_URL")
+     '''
+
+  @expose
+  def logged(code = None,
+             error_reason = None,
+             error = None,
+             error_description = None):
+    if error: return "Login Failure."
+    if code:
+      external = urlopen(
+        "https://graph.facebook.com/oauth/access_token?" +
+        "client_id=YOUR_APP_ID"&
+        "redirect_uri=YOUR_URL"
+        "&client_secret=YOUR_APP_SECRET&"
+        "code=" + code)
+      token_query = json.loads(external.read())
+      external.close()
+      try:
+        token = token_query['access_token']
+        query_site = urlopen(
+          'https://graph.facebook.com/me?access_token=ACCESS_TOKEN')
+        query = json.loads(query_site.read())
+        query_site.close()
+        id = query['id']
+        return number
+        
+      except IndexError:
+        return "Failed"
+
 '''
     # something for loading page with groups
     @expose
