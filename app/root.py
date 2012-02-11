8e6e3e42f2f0d6e69a38ee06d07ce54f08e49972
Maryam Aly <juggalettethreat@gmail.com>
2012-02-11 10:47:53 -0500

    removing models again

--- app/root.py
+++ app/root.py
@@ -7,13 +7,14 @@ from cherrypy import expose
 
 import sqlalchemy
 
+'''
 from model import user 
 from model import link 
 from model import group 
+'''
 
 from genshi.template import TemplateLoader
 loader = TemplateLoader('app/view/templates', auto_reload=True)
-
 
 class Root(object):
 
