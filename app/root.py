f385e9cd3fd3f277a19aa7847b268f7108b70b6a
Maryam Aly <juggalettethreat@gmail.com>
2012-02-11 05:31:50 -0500

    changed root

--- app/root.py
+++ app/root.py
@@ -12,4 +12,17 @@ class Root(object):
         tmpl = loader.load('index.html')
         page = tmpl.generate(links=True)
         return page.render('html', doctype='html')
-        return 'Test'
+    
+    # something for loading page with groups
+    @expose
+    def index2(*args, **dargs):
+        tmpl = loader.load('index2.html')
+        page = tmpl.generate(links=True)
+        return page.render('html', doctype='html')
+    
+    # something for loading page with links
+    @expose
+    def index3(*args, **dargs):
+        tmpl = loader.load('index3.html')
+        page = tmpl.generate(links=True)
+        return page.render('html', doctype='html')
